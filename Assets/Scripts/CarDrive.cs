899ce2a8f9d8da6dfd8bd6849a172b1fdd6b28c3
John Buckland <jbuckland@cityworks.com>
2018-03-31 18:09:57 -0600

    initial commit

---
+++ Assets/Scripts/CarDrive.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarDrive : MonoBehaviour
+{
+
+    private Vector3 moveDirection;
+    public float speed;
+    public float maxRotation;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    private void FixedUpdate()
+    {
+        transform.position += transform.forward * speed * Time.deltaTime;
+
+        var rotationDirection = Random.Range(-1f, 1f);
+        var rotation = maxRotation * rotationDirection;
+
+        transform.Rotate(new Vector3(0, rotation * Time.deltaTime, 0));
+
+
+    }
+}
