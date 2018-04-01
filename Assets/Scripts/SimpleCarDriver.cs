d3108d7330036fd99a6b9360049593c534fec357
John Buckland <jbuckland@cityworks.com>
2018-04-01 11:15:32 -0600

    Added some UI to view inputs to the simple car.

--- Assets/Scripts/SimpleCarDriver.cs
+++ Assets/Scripts/SimpleCarDriver.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SimpleCarDriver : MonoBehaviour
 {
@@ -9,11 +10,14 @@ public class SimpleCarDriver : MonoBehaviour
     public float maxSpeed;
     public Rigidbody carBody;
 
+    public Text speedText;
+    public Text turnText;
+
     // Update is called once per frame
     void Update()
     {
-        float verticalInput = Input.GetAxis("Vertical");
         float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
 
         var rotation = maxTurn * horizontalInput * Time.deltaTime;
         if (rotation > 0f)
@@ -21,11 +25,11 @@ public class SimpleCarDriver : MonoBehaviour
             Debug.Log("rotating " + rotation);
         }
         transform.Rotate(new Vector3(0, rotation, 0));
+        turnText.text = (horizontalInput * 100).ToString("0");
 
 
         var moveForce = new Vector3(0, 0, maxSpeed * verticalInput * Time.deltaTime);
         carBody.AddRelativeForce(moveForce, ForceMode.VelocityChange);
-
-
+        speedText.text = (verticalInput * 100).ToString("0");
     }
 }
